b8ecb9d7b151d8bfb1fa0aadb21f67d0b258a050
Aakarsh Lohani <lohaniaakarsh2017@gmail.com>
2025-07-24 23:14:20 +0530

    message placeholder

---
+++ Prompt/message_placeholder.py
@@ -0,0 +1,18 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+# chat template
+chat_template = ChatPromptTemplate([
+    ('system','you are a helpful assistant'),
+    MessagesPlaceholder(variable_name='chat_history'),
+    ('human','query')
+])
+
+
+#load chat history
+chat_history = []
+
+with open('chat_history.txt') as f:
+    chat_history.extend(f.readlines())
+
+#create prompt
+print(chat_history)
