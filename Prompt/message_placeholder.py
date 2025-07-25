0e0da1f33d681463d167253a3a929e2d2b01ba9e
Aakarsh Lohani <lohaniaakarsh2017@gmail.com>
2025-07-25 12:31:20 +0530

    dynamic prompt completed

--- Prompt/message_placeholder.py
+++ Prompt/message_placeholder.py
@@ -4,7 +4,7 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 chat_template = ChatPromptTemplate([
     ('system','you are a helpful assistant'),
     MessagesPlaceholder(variable_name='chat_history'),
-    ('human','query')
+    ('human','{query}')
 ])
 
 
@@ -15,4 +15,8 @@ with open('chat_history.txt') as f:
     chat_history.extend(f.readlines())
 
 #create prompt
-print(chat_history)
+# print(chat_history)
+
+prompt = chat_template.invoke({'chat_history':chat_history,'query':'What is the status ?'})
+
+print(prompt)
