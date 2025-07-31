7404d33e6735c090681223fb061bcb8985baff49
Aakarsh Lohani <lohaniaakarsh2017@gmail.com>
2025-07-31 23:51:17 +0530

    String Output parser completed

--- Output_Parser/string_outputparser.py
+++ Output_Parser/string_outputparser.py
@@ -5,3 +5,27 @@ from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
+llm = HuggingFaceEndpoint(
+    repo_id='meta-llama/Meta-Llama-3-8B-Instruct',
+    task= 'text-generation'
+)
+
+model = ChatHuggingFace(llm = llm)
+
+template1 = PromptTemplate(
+    template='Give your views on {topic}',
+    input_variables=['topic']
+)
+
+template2 = PromptTemplate(
+    template='Give 5 line summary on {text}',
+    input_variables=['text']
+)
+
+parser = StrOutputParser()
+
+chain = template1 | model | parser | template2 | model
+
+result = chain.invoke({'topic':'AI'})
+
+print(result.content)
