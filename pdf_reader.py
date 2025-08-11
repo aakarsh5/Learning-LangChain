5930b45669ec811bf2a6ffcded42c97a4fec9391
Aakarsh Lohani <lohaniaakarsh2017@gmail.com>
2025-08-11 23:43:32 +0530

    up to vector store completed

--- pdf_reader.py
+++ pdf_reader.py
@@ -3,6 +3,8 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
 
 # Load Document
 loader = TextLoader('doc.txt')
@@ -11,4 +13,14 @@ documents = loader.load()
 # Split the document into chunks
 text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overloap = 50)
 docs = text_splitter.split_documents(documents)
+
+# Convert text into embeddings and store in FAISS store
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformer/all-MiniLM-L6-v2")
+vector_store = FAISS.from_documents(docs,embeddings)
+
+# Create a retriever
+retriever = vector_store.as_retriever()
+
+
+
 
