81d09a6c5307d3b8027d520edd6da94a30d0d07e
Aakarsh Lohani <lohaniaakarsh2017@gmail.com>
2025-08-16 18:27:45 +0530

    Lambda Function Completed

--- Runnables/runnable_lambda.py
+++ Runnables/runnable_lambda.py
@@ -1,6 +1,6 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnableParallel, RunnableSequence, RunnablePassthrough
+from langchain_core.runnables import RunnableParallel, RunnableSequence, RunnablePassthrough, RunnableLambda
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 
@@ -12,3 +12,34 @@ llm= HuggingFaceEndpoint(
 )
 
 model = ChatHuggingFace(llm = llm)
+
+parser = StrOutputParser()
+
+prompt = PromptTemplate(
+    template='Give a joke on {topic}',
+    input_variables=['topic']
+)
+
+def word_count(text):
+    return len(text.split())
+
+chain = RunnableSequence(prompt, model, parser)
+
+parallel_chain = RunnableParallel({
+    'joke':RunnablePassthrough(),
+    'word_count':RunnableLambda(word_count)
+})
+
+# direct function
+# parallel_chain = RunnableParallel({
+#     'joke':RunnablePassthrough(),
+#     'word_count':RunnableLambda(lambda x: len(x.split()))
+# })
+
+final_chain = RunnableSequence(chain, parallel_chain)
+
+result = final_chain.invoke({'topic':'AI'})
+
+final_result = """{} \n Word Count {}""".format(result['joke'], result['word_count'])
+
+print(final_result)
