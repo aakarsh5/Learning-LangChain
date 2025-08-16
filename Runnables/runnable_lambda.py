7fe77a131db924f85d8d70b366449ef8fb7c6cc1
Aakarsh Lohani <lohaniaakarsh2017@gmail.com>
2025-08-16 14:31:54 +0530

    Runnable Lambda

---
+++ Runnables/runnable_lambda.py
@@ -0,0 +1,14 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel, RunnableSequence, RunnablePassthrough
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm= HuggingFaceEndpoint(
+    repo_id='google/gemma-2-2b-it',
+    task='text-generation'
+)
+
+model = ChatHuggingFace(llm = llm)
